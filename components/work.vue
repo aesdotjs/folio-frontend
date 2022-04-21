e6d102130bead91028776283cc71becd61d60eab
aesdotjs <skriabin.simon@gmail.com>
2022-04-21 18:09:01 +0200

    Swiper switch webp

--- components/work.vue
+++ components/work.vue
@@ -136,9 +136,6 @@ const getImageSizes = function () {
 const transformImage = function (image, option) {
   if (!image) return "";
   if (!option) return "";
-
-  let imageService = "https://img2.storyblok.com/";
-  let path = image.replace("https://a.storyblok.com", "");
-  return imageService + option + path;
+  return image + "/m/" + option;
 };
 </script>
